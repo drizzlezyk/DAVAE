a0df8b85c1469e0189322e643cd55d734044a679
drizzlezyk <43944770+drizzlezyk@users.noreply.github.com>
2021-01-04 20:29:16 +0800

    uopdate docs

--- setup.py
+++ setup.py
@@ -2,15 +2,14 @@ from distutils.core import setup
 from setuptools import setup, find_packages
 
 install_requires = [
-      'numpy',
-      'scanpy',
-      'tensorflow',
-      'scipy',
-      'pandas',
-      'seaborn',
-      'python-igraph',
-      'louvain',
-      'desc',
+      'numpy==1.18.5',
+      'scanpy==1.5.1',
+      'tensorflow==2.3.0',
+      'scipy==1.4.1',
+      'pandas==1.1.0',
+      'python-igraph==0.8.2',
+      'louvain==0.7.0',
+      'desc==2.1.1',
 ]
 
 setup(
